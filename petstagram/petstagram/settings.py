898eed773b716f40a20f55e921ffa906170d81a7
angeluzunov10 <angel_d_u@abv.bg>
2024-10-09 18:49:52 +0300

    Setting up apps views and urls.

--- petstagram/petstagram/settings.py
+++ petstagram/petstagram/settings.py
@@ -33,7 +33,7 @@ MY_APPS = [
     "petstagram.photos",
     "petstagram.pets",
     "petstagram.accounts",
-    "petstagram.common"
+    "petstagram.common",
 ]
 
 INSTALLED_APPS = [
@@ -60,8 +60,9 @@ ROOT_URLCONF = 'petstagram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
