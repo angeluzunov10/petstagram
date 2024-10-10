98cdd5179679c9fd148b3370a885b57170608d7e
angeluzunov10 <angel_d_u@abv.bg>
2024-10-10 20:11:16 +0300

    Added Pet model

--- petstagram/petstagram/accounts/urls.py
+++ petstagram/petstagram/accounts/urls.py
@@ -4,7 +4,7 @@ from petstagram.accounts import views
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
-    path('profile/<int:pk>/>', include([
+    path('profile/<int:pk>/', include([
         path('', views.profile_details, name='profile-details'),
         path('edit/', views.profile_edit, name='profile-edit'),
         path('delete/', views.profile_delete, name='profile-delete'),
