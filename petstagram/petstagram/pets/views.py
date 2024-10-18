4e6947caf485b4e3cab6c2a844748e5d15515bfd
angeluzunov10 <angel_d_u@abv.bg>
2024-10-18 17:25:59 +0300

    Added the pet add and edit forms

--- petstagram/petstagram/pets/views.py
+++ petstagram/petstagram/pets/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from petstagram.pets.forms import PetAddForm, PetEditForm
 from petstagram.pets.models import Pet
 
 
@@ -7,11 +8,35 @@ from petstagram.pets.models import Pet
 
 
 def pet_add_page(request):
-    return render(request, 'pets/pet-add-page.html')
+    form = PetAddForm(request.POST or None)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('profile-details', pk=1)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'pets/pet-add-page.html', context)
 
 
 def pet_edit_page(request, username, pet_slug):
-    return render(request, 'pets/pet-edit-page.html')
+    pet = Pet.objects.get(slug=pet_slug)
+    form = PetEditForm(request.POST or None, instance=pet)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('pet-details', username, pet_slug)
+
+    context = {
+        'pet': pet,
+        'form': form,
+    }
+
+    return render(request, 'pets/pet-edit-page.html', context)
 
 
 def pet_delete_page(request, username, pet_slug):
