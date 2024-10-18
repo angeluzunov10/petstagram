840de239db74e4de20b4f857b3d66a94eab291e5
angeluzunov10 <angel_d_u@abv.bg>
2024-10-18 21:30:07 +0300

    Added the photo edit form and the delete photo functionality.

--- petstagram/petstagram/photos/views.py
+++ petstagram/petstagram/photos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from petstagram.photos.forms import PhotoAddForm
+from petstagram.photos.forms import PhotoAddForm, PhotoEditForm
 from petstagram.photos.models import Photo
 
 
@@ -23,7 +23,20 @@ def photo_add_page(request):
 
 
 def photo_edit_page(request, pk):
-    return render(request, 'photos/photo-edit-page.html')
+    photo = Photo.objects.get(pk=pk)
+    form = PhotoEditForm(request.POST or None, instance=photo)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('photo-details', pk)
+
+    context = {
+        'form': form,
+        'photo': photo,
+    }
+
+    return render(request, 'photos/photo-edit-page.html', context)
 
 
 def photo_details_page(request, pk):
@@ -38,3 +51,9 @@ def photo_details_page(request, pk):
     }
 
     return render(request, 'photos/photo-details-page.html', context)
+
+
+def photo_delete(request, pk):
+    Photo.objects.get(pk=pk).delete()
+
+    return redirect('home')
