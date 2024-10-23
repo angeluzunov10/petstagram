98e74125362c9ad77eb1d16b518bfd1c594c2926
angeluzunov10 <angel_d_u@abv.bg>
2024-10-23 20:50:21 +0300

    Added css for page buttons. Used CBVs for home page and pets pages

--- petstagram/petstagram/common/views.py
+++ petstagram/petstagram/common/views.py
@@ -1,4 +1,6 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, redirect, resolve_url
+from django.views.generic import ListView
 from pyperclip import copy
 
 from petstagram.common.forms import CommentForm, SearchForm
@@ -6,29 +8,62 @@ from petstagram.common.models import Like
 from petstagram.photos.models import Photo
 
 
-def home_page(request):
-    all_photos = Photo.objects.all()
-    comment_form = CommentForm()
-    search_form = SearchForm(request.GET)
+class HomePage(ListView):
+    model = Photo
+    template_name = 'common/home.html'
+    context_object_name = 'all_photos'
+    paginate_by = 1
+    paginator_class = Paginator
 
-    if search_form.is_valid():
-        all_photos = all_photos.filter(
-            tagged_pets__name__icontains=search_form.cleaned_data['pet_name'],
-        )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
 
-    context = {
-        'all_photos': all_photos,
-        'comment_form': comment_form,
-        'search_form': search_form
-    }
+        context['comment_form'] = CommentForm()
+        context['search_form'] = SearchForm(self.request.GET)
 
-    return render(request, 'common/home.html', context)
+        return context
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        pet_name = self.request.GET.get('pet_name')
+
+        if pet_name:
+            queryset = queryset.filter(
+                tagged_pets__name__icontains=pet_name
+            )
+
+        return queryset
+
+
+# def home_page(request):
+#     all_photos = Photo.objects.all()
+#     comment_form = CommentForm()
+#     search_form = SearchForm(request.GET)
+#
+#     if search_form.is_valid():
+#         all_photos = all_photos.filter(
+#             tagged_pets__name__icontains=search_form.cleaned_data['pet_name']
+#         )
+#
+#     photos_per_page = 1
+#     paginator = Paginator(all_photos, photos_per_page)
+#     page_number = request.GET.get('page')
+#
+#     all_photos = paginator.get_page(page_number)
+#
+#     context = {
+#         'all_photos': all_photos,
+#         'comment_form': comment_form,
+#         'search_form': search_form
+#     }
+#
+#     return render(request, 'common/home.html', context)
+#
 
 def likes_functionality(request, photo_id):
     liked_object = Like.objects.filter(
-        to_photo_id=photo_id,
-    )
+        to_photo_id=photo_id
+    ).first()
 
     if liked_object:
         liked_object.delete()
--- petstagram/petstagram/settings.py
+++ petstagram/petstagram/settings.py
@@ -130,11 +130,11 @@ USE_TZ = True
 STATIC_URL = 'petstagram/static/'
 
 STATICFILES_DIRS = (
-    BASE_DIR / 'static',
+    BASE_DIR / 'static/',
 )
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / ''
+MEDIA_URL = 'static/images/'
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
