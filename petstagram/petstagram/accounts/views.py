b9f1788e1fde1062de6faee20fd8a86bc2a000bc
angeluzunov10 <angel_d_u@abv.bg>
2024-11-16 18:23:14 +0200

    Made an AppUserRegisterView and made some changes in the register-page template.

--- petstagram/petstagram/accounts/views.py
+++ petstagram/petstagram/accounts/views.py
@@ -1,14 +1,27 @@
+from django.contrib.auth import get_user_model, login
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from petstagram.accounts.forms import AppUserCreationForm
+
+UserModel = get_user_model()
+
+# def login(request):
+#     return render(request, 'accounts/login-page.html')
 
 
-def login(request):
-    return render(request, 'accounts/login-page.html')
+class AppUserRegisterView(CreateView):
+    form_class = AppUserCreationForm
+    template_name = 'accounts/register-page.html'
+    success_url = reverse_lazy('home')
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
 
-def register(request):
-    return render(request, 'accounts/register-page.html')
+        login(self.request, self.object)
+
+        return response
 
 
 def profile_delete(request, pk):
