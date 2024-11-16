324f39f4ab06d64f17c7a91aa9e3e904ad2fd999
angeluzunov10 <angel_d_u@abv.bg>
2024-11-16 19:26:30 +0200

    Added LogoutView and changed the logout button.

--- petstagram/petstagram/accounts/urls.py
+++ petstagram/petstagram/accounts/urls.py
@@ -1,14 +1,14 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 from petstagram.accounts import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login/', views.AppUserLoginView.as_view(), name='login'),
     path('register/', views.AppUserRegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/<int:pk>/', include([
         path('', views.profile_details, name='profile-details'),
         path('edit/', views.profile_edit, name='profile-edit'),
         path('delete/', views.profile_delete, name='profile-delete'),
-    ])),
-    path('login/', views.login, name='login'),
-
+    ]))
 ]
